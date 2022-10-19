fdbeae303d4a09666c948519927c607563134bae
Daniel John McLaughlan <daniel-j-m@live.com>
2022-10-19 20:33:44 +0100

    Working on Health and fixed issue with eye

---
+++ I.See.You/Assets/HealthUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public Slider Bar;
+    public GameObject Player;
+    public PlayerHealth HealthScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        HealthScript = Player.GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SliderValue (int Health)
+    {
+        Bar.value = HealthScript.Health / 2;
+    }
+
+}
